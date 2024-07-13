df5d246204fb8955de77ffb66b0532fc00a8bf1c
shadowrahlm <lordfreizarahl@gmail.com>
2024-07-13 18:33:34 +0300

    updated and tested no bugs this time

--- exe.py
+++ exe.py
@@ -1,35 +1,32 @@
-#! /usr/bin/python3.12 
+#!/usr/bin/python3.12
 
 import subprocess as sp
-import os 
-import dpkg #type: ignore
-#directory = sys.argv[1]
-directory = '/media/kali/Doomsday/tools'
+import os
 
 def cwd_and_exec(folder):
-    
-    #change to the folder where the package to install is 
-    #cwd = os.getcwd()#this can be one way to do this 
-    #os.path.join(cwd,folder)
-    
+    # Change directory to the specified folder
     os.chdir(folder)
+    
+    # Create the directory if it doesn't exist
     if not os.path.exists(folder):
         os.makedirs(folder)
-        #this will extract the contents of the .deb file to a directory
-    with dpkg.DebFile(folder/'code.deb') as deb:
-        deb.extract()
-     
+    
+    # Assuming 'code.deb' is located in the folder, extract its contents
+    # Adjust this part based on the correct location and extraction method
+    deb_file = os.path.join(folder, 'code.deb')
+    if os.path.isfile(deb_file):
+        sp.run(['dpkg', '-x', deb_file, folder])
+    
+    # Installation directory path
     install_dir = '/opt'
-    os.path.join(folder,install_dir)
+    install_path = os.path.join(folder, install_dir)
     
-    sp.run(['dpkg', '-i',f'{install_dir}.dir'], check=True)
-    exec = sp.run('cd ..;cp -rv .vscode /home/kali'';''exit',shell=True,)
+    # Install the package using dpkg
+    sp.run(['dpkg', '-i', install_path], check=True)
     
-    
-    
-    
-    
-    
-if __name__=='__main__':
-    cwd_and_exec(directory)    
+    # Example of copying '.vscode' directory to '/home/kali'
+    sp.run(['cp', '-rv', '.vscode', '/home/kali'])
 
+if __name__ == '__main__':
+    directory = '/media/kali/Doomsday/tools'  # Replace with your actual directory
+    cwd_and_exec(directory)
--- tsts.py
+++ tsts.py
@@ -1,138 +1,114 @@
-#! /usr/bin/python3.12
+#!/usr/bin/python3.12
+
 import re
 
+# Example 1: Updating wardrobe with new items
+wardrobe = {'shirt': ['red', 'blue', 'white'], 'jeans': ['blue', 'black']}
+new_items = {'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown']}
+wardrobe.update(new_items)
+print("Updated wardrobe:", wardrobe)
 
-# wardrobe = {'shirt': ['red', 'blue', 'white'], 'jeans': ['blue', 'black']}
-# new_items = {'jeans': ['white'], 'scarf': ['yellow'], 'socks': ['black', 'brown']}
-# wardrobe.update(new_items)
-# print(wardrobe)
-    
-    
+# Example 2: Using a while loop to print characters of a string
+x = 'spam'
+while x:
+    print(x, end=' ')
+    x = x[1:]  # Remove the first character each iteration
 
-# x = 'spam'
+# Example 3: Using a while loop with continue and even numbers
+x = 10
+while x:
+    x -= 1
+    if x % 2 != 0:
+        continue  # Skip odd numbers
+    print(x, end=' ')
 
-# while x:
-#     print(x, end=' ')
-#     x = x[1:] # += results into increament which the undesired effect here
-    
-    
-    
-# x = 10
-# while x:
-#     x -= 1
-#     if x % 2 != 0: continue
-#     print(x ,end=' ')
-       
+# Example 4: Checking if a number is prime using while-else loop
+y = 5
+x = y // 2
+while x > 1:
+    if y % x == 0:
+        print(y, 'has factor', x)
+        break
+    x -= 1
+else:
+    print(y, 'is prime')
 
-# y = 5
+# Example 5: Checking membership in a list using for loop
+items = ["aaa", 111, (4, 5), 2.01]
+tests = [(4, 5), 2.01, 'aa']
 
-# x = y // 2
+for key in tests:
+    if key in items:
+        print(key, "was found")
+    else:
+        print(key, "not found!")
 
+# Example 6: Using enumerate to get index and value from a list
+couped = [5, 3545, 464, 64, 64, 4, 543, 464, 54, 64, 654, 64, 6878, 7 * 68 * 2 // 4, 76, 4 + 7, 64]
+for i, x in enumerate(couped):
+    print(f'{x} is at index {i}')
 
-
-# while x > 1:
-#     if y % x == 0:
-#         print(y, 'has factor', x)
-#         break
-#     x -= 1
-# else:
-#     print(y ,'is prime')    
-    
-
-
-# items = ["aaa", 111, (4, 5), 2.01]
-# tests = [(4, 5), 2.01,'aa']
-    
-    
-# for key in tests:
-
-#     if key in items:
-
-#         print(key, "was found")
-#     else:
-#         print(key, "not found!")
-
-#couped = [5,3545,464,64,64,4,543,464,54,64,654,64,6878,7*68*2//4,76,4+7,64,]
-# current_index = 0
-
-# for i in couped:
-#     print(i ,'is at current index',current_index) #explict way to get offset and value 
-#     current_index +=1
-# for i ,x in enumerate(couped):#implict way to get offset and value
-#     result = f'{x} => is at index:{i}'
-#     print(result)
-    
-    
-# named_list=[f'{x} is at index {i}'for i,x in enumerate(couped)]#list comphression version 
-# print(named_list)
-
+# Example 7: Finding a number in a list using different methods
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
-# found = False
-# i = 0
-# while not found and i < len(L):
-#     if 2 ** X == L[i]:
-#         found = True
-#     else:
-#         i +=1
-# if found:
-#     print('at index', i)
-# else:
-#     print(X, 'not found')
 
-# for x in L:
-#     if 2**X == x:
-#         print(2**X,'was found at',L.index(x))
-#         break
-#     else:
-#         print(X,'not found')
-        
-        
-        
-        
-# if 2 ** X in L:
-#     print(2 ** X, 'was found at', L.index(2 ** X))
-# else:
-#     print(X, 'not found')
+# Method 1: Using a while loop
+found = False
+i = 0
+while not found and i < len(L):
+    if 2 ** X == L[i]:
+        found = True
+    else:
+        i += 1
+if found:
+    print('At index', i)
+else:
+    print(X, 'not found')
 
+# Method 2: Using a for loop with break
+for x in L:
+    if 2 ** X == x:
+        print(2 ** X, 'was found at', L.index(x))
+        break
+else:
+    print(X, 'not found')
 
-# st = 'spam'
+# Method 3: Using membership check
+if 2 ** X in L:
+    print(2 ** X, 'was found at', L.index(2 ** X))
+else:
+    print(X, 'not found')
 
-# for i  in st:
-#     print(ord(i))
-    
-# x=0
-# for i in st:x+=ord(i)
-# print(x)
+# Example 8: Using ord() to get ASCII values of characters in a string
+st = 'spam'
+for i in st:
+    print(ord(i))
 
+# Example 9: Calculating the sum of ASCII values using a for loop
+x = 0
+for i in st:
+    x += ord(i)
+print(x)
 
-# x = []
-# for c in st: x.append(ord(c))
-# print(x)
+# Example 10: Creating a list of ASCII values using list comprehension
+x = [ord(c) for c in st]
+print(x)
 
-x='spam'
+# Example 11: Function example accessing global variable
+x = 'spam'
 
 def func():
     print(x)
 
+func()
 
-# func()
-
-
-
+# Example 12: Using regular expressions to find a pattern in a string
 def find_isbn(lst):
-     pattern = r"\b(\d{3})-(\d)-(\d{2}-(\d{6}-(\d)\b))"
-     result = re.search(pattern,lst)
-     if result is not None:
-         return ''
-     ans = f"{result[4]}"
-     return (ans)
-    #pattern = r"\d"
-    #result = re.findall(pattern,lst)
-    #number_str = re.sub(r'\D','',''.join(map(str,result)))
-    
-
-
+    pattern = r"\b(\d{3})-(\d)-(\d{2}-(\d{6}-(\d)\b))"
+    result = re.search(pattern, lst)
+    if result is not None:
+        return result.group(4)  # Return the 4th group if pattern matches
+    return ''
 
 print(find_isbn("123-4-12-098754-0"))
 print(find_isbn("223094-AB-12-30"))
