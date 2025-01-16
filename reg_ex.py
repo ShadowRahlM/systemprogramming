87c2764766d4e30b7f844ce2a660138c6a43cd51
shadowrahlm <lordfreizarahl@gmail.com>
2025-01-16 18:58:18 +0300

    Add new regex pattern to reg_ex.py for matching sequences of uppercase letters followed by 'a'

--- reg_ex.py
+++ reg_ex.py
@@ -6,3 +6,10 @@ pattern = r'Bat(wo)+.*'
 text = 'The Adenture of Batwowo_man Batman'
 result= re.search(pattern,text)
 print(result.group())
+
+
+
+pattern = r'([A-Z]+a){3,}'
+text = 'HaHaHaCaCaCaSbSbbSTaTaDaDaDab'
+query = re.search(pattern,text)
+print(query.group())
